e1762e316fbedfa0129bc0140e6ae939cc3e72db
Aidan Low <aidan.low98@gmail.com>
2018-10-31 20:44:58 -0400

    added collapsible functionality and updated homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,11 +4,63 @@ class Homepage extends Component {
   render() {
     return (
       <div className="Homepage">
-        <h3>WARNING: THE FOUNDATION DATABASE IS</h3>
-        <h1>CLASSIFIED</h1>
-        <h4>ACCESS BY UNAUTHORIZED PERSONNEL IS<br />
-        STRICTLY PROHIBITED</h4>
-        <h4>PERPETRATORS WILL BE TRACKED, LOCATED, AND DETAINED</h4>
+        <div className="homepage-header">
+          <h3>WARNING: THE FOUNDATION DATABASE IS</h3>
+          <h1>CLASSIFIED</h1>
+          <h4>ACCESS BY UNAUTHORIZED PERSONNEL IS<br />
+          STRICTLY PROHIBITED</h4>
+          <h4>PERPETRATORS WILL BE TRACKED, LOCATED, AND DETAINED</h4>
+        </div>
+        <div className="container card">
+          <div className="card-body">
+            Hi! Im ALowVerus, and Im trying to create a port of the Foundation
+            website off the WikiDot site.
+          </div>
+          <div className="row card-body" >
+          <h3 className="col-md-12">Goals:</h3>
+            <div className="col-md-12 card">
+                Complete port of Wikidot site votes, comments, tags, and other data
+            </div>
+            <div className="col-md-6 card">
+                Smooth page transitions
+            </div>
+            <div className="col-md-6 card">
+                Full user login functionality
+            </div>
+            <div className="col-md-6 card">
+                Full sandbox functionality
+            </div>
+            <div className="col-md-6 card">
+                Retention of Wikidot markup functionality
+            </div>
+          </div>
+          <div className="card-body">
+            <p>
+              The Rails API repo is available at:
+            </p>
+            <h5>
+              <a href="https://github.com/ALowVerus/scp-rails-api" target="_blank" rel="noopener noreferrer">
+                "https://github.com/ALowVerus/scp-rails-api"
+              </a>
+            </h5>
+            <p>
+              The React front-end repo is available at:
+            </p>
+            <h5>
+              <a href="https://github.com/ALowVerus/scp-react" target="_blank" rel="noopener noreferrer">
+                "https://github.com/ALowVerus/scp-react"
+              </a>
+            </h5>
+            <p>
+              Progress is tracked at Trello:
+            </p>
+            <h5>
+              <a href="https://trello.com/b/xosE6utC/scp-website" target="_blank" rel="noopener noreferrer">
+                "https://trello.com/b/xosE6utC/scp-website"
+              </a>
+            </h5>
+          </div>
+        </div>
       </div>
     )
   }
--- src/Page.js
+++ src/Page.js
@@ -6,7 +6,9 @@ class Page extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      page_id: "",
       innerHTML: "",
+      collapsibleToggles: [],
       pageTags: [],
       discussOpened: false
     };
@@ -15,6 +17,7 @@ class Page extends Component {
 
   componentDidMount() {
     console.log(this.props.url);
+    // Grab actual page
     axios
       .get(
         this.props.db_address + "pages?url=" + this.props.url,
@@ -22,35 +25,92 @@ class Page extends Component {
       )
       .then(response => {
         this.setState({
+          page_id: response.data[0].id,
           innerHTML: response.data[0].html,
-          pageTags: response.data[1]
+          pageTags: response.data[1],
         });
         console.log(response);
-        // Check for yui boxes, evade the null scenario
-        var yui_sets = document.getElementsByClassName('yui-navset');
-        if (yui_sets !== null) {
-          let yui_set, yui_nav_ul, yui_nav, yui_content;
-          // Iterate through the navs of each set to find the active tabs
-          for (var yui_set_count = 0; yui_set_count < yui_sets.length; yui_set_count ++) {
-            yui_set = yui_sets[yui_set_count];
-            yui_nav_ul = yui_set.getElementsByClassName('yui-nav')[0];
-            yui_nav_ul.className = "yui-nav nav navbar-nav";
-            yui_nav = yui_nav_ul.children;
-            yui_content = yui_set.getElementsByClassName('yui-content')[0].children;
-            // Give each nav and tab and appropriate ID for testing purposes
-            for (var tab_count = 0; tab_count < yui_nav.length; tab_count ++) {
-              yui_nav[tab_count].onclick = (event) => { this.updateTabs(event); }
-              yui_nav[tab_count].id = "nav-"+ yui_set_count.toString() + "-" + tab_count.toString()
-              yui_nav[tab_count].innerHTML = yui_nav[tab_count].textContent;
-              yui_content[tab_count].id = "content-"+ yui_set_count.toString() + "-" + tab_count.toString()
-            }
-          }
-        }
+        // Conform page to react standards
+        this.sanitizePage();
+        // Grab page rating
+        this.grabAggregateRatings(response.data[0].id);
       })
       .catch(error => {
         this.setState({ innerHTML: "ERROR 404: Page not found." })
         console.log(error);
       });
+  }
+
+  grabAggregateRatings(page_id) {
+    // axios
+    //   .get(
+    //     this.props.db_address + "ratings?url=" + this.props.url,
+    //     {headers: {"Access-Control-Allow-Origin": "*"}}
+    //   )
+    //   .then(response => {
+    //     this.setState({
+    //       innerHTML: response.data[0].html,
+    //       pageTags: response.data[1]
+    //     });
+    //     console.log(response);
+    //     this.sanitizePage();
+    //   })
+    //   .catch(error => {
+    //     this.setState({ innerHTML: "ERROR 404: Page not found." })
+    //     console.log(error);
+    //   });
+  }
+
+  sanitizePage() {
+    // SANITIZE YUIs
+    // Check for yui boxes, evade the null scenario
+    var yui_sets = document.getElementsByClassName('yui-navset');
+    if (yui_sets !== null) {
+      let yui_set, yui_nav_ul, yui_nav, yui_content;
+      // Iterate through the navs of each set to find the active tabs
+      for (var yui_set_count = 0; yui_set_count < yui_sets.length; yui_set_count ++) {
+        yui_set = yui_sets[yui_set_count];
+        yui_nav_ul = yui_set.getElementsByClassName('yui-nav')[0];
+        yui_nav_ul.className = "yui-nav nav navbar-nav";
+        yui_nav = yui_nav_ul.children;
+        yui_content = yui_set.getElementsByClassName('yui-content')[0].children;
+        // Give each nav and tab and appropriate ID for testing purposes
+        for (var tab_count = 0; tab_count < yui_nav.length; tab_count ++) {
+          yui_nav[tab_count].onclick = (event) => { this.updateTabs(event); }
+          yui_nav[tab_count].id = "nav-"+ yui_set_count.toString() + "-" + tab_count.toString()
+          yui_nav[tab_count].innerHTML = yui_nav[tab_count].textContent;
+          yui_content[tab_count].id = "content-"+ yui_set_count.toString() + "-" + tab_count.toString()
+        }
+      }
+    }
+    // SANITIZE COLLAPSIBLES
+    var collapsibles = document.getElementsByClassName('collapsible-block');
+    if (collapsibles !== null) {
+      let collapsible, collapsible_folded, collapsible_unfolded, links_folded, links_unfolded;
+      for (var collapsible_count = 0; collapsible_count < collapsibles.length; collapsible_count ++) {
+        collapsible = collapsibles[collapsible_count];
+        collapsible.id = "collapsible-" + collapsible_count.toString();
+        collapsible_folded = collapsible.getElementsByClassName("collapsible-block-folded")[0];
+        collapsible_unfolded = collapsible.getElementsByClassName("collapsible-block-unfolded")[0];
+        collapsible_folded.id = "collapsible-" + collapsible_count.toString() + "-folded";
+        collapsible_unfolded.id = "collapsible-" + collapsible_count.toString() + "-unfolded";
+        links_folded = collapsible_folded.getElementsByClassName("collapsible-block-link");
+        links_unfolded = collapsible_unfolded.getElementsByClassName("collapsible-block-unfolded-link");
+        for (let link_count = 0; link_count < links_folded.length; link_count ++) {
+          links_folded[link_count].innerHTML = links_folded[link_count].textContent;
+          links_folded[link_count].id = "folded-link-" + collapsible_count.toString() + "-" + link_count.toString();
+          links_folded[link_count].onclick = (event) => { this.switchCollapse(event); };
+        }
+        for (let link_count = 0; link_count < links_unfolded.length; link_count ++) {
+          var actual_link = links_unfolded[link_count].getElementsByClassName('collapsible-block-link')[0];
+          actual_link.id = "unfolded-link-" + collapsible_count.toString() + "-" + link_count.toString();
+          actual_link.onclick = (event) => { this.switchCollapse(event); };
+        }
+        const newCollapsibleToggles = this.state.collapsibleToggles;
+        newCollapsibleToggles.push(false);
+        this.setState({ collapsibleToggles: newCollapsibleToggles });
+      }
+    }
   }
 
   updateTabs(event){
@@ -86,6 +146,27 @@ class Page extends Component {
     yui_content_new.style = "";
   }
 
+  switchCollapse(event) {
+    event = event || window.event; // IE
+    var target = event.target || event.srcElement; // IE
+    var collapsible_index = target.id.split("-")[2];
+    var collapsible_folded = document.getElementById("collapsible-" + collapsible_index.toString() + "-folded");
+    var collapsible_unfolded = document.getElementById("collapsible-" + collapsible_index.toString() + "-unfolded");
+    if (this.state.collapsibleToggles[collapsible_index] === true) {
+      collapsible_folded.style = "display:block";
+      collapsible_unfolded.style = "display:none";
+      const newCollapsibleToggles = this.state.collapsibleToggles;
+      newCollapsibleToggles[collapsible_index] = false;
+      this.setState({ collapsibleToggles: newCollapsibleToggles });
+    } else {
+      collapsible_folded.style = "display:none";
+      collapsible_unfolded.style = "display:block";
+      const newCollapsibleToggles = this.state.collapsibleToggles;
+      newCollapsibleToggles[collapsible_index] = true;
+      this.setState({ collapsibleToggles: newCollapsibleToggles });
+    }
+  }
+
   discussClicked() {
     var discussion = document.getElementById('discussion');
     if (this.state.discussOpened === true) {
@@ -108,17 +189,29 @@ class Page extends Component {
       <div className="Page">
         <div className="Page-html col-12" dangerouslySetInnerHTML={{__html:this.state.innerHTML}} />
         <div className="Page-footer">
-          <div className="d-flex flex-wrap btn btn-secondary justify-content-around">
+          <div className="d-flex flex-wrap justify-content-around">
             {this.state.pageTags.map(function(pageTag){return(
               <div className="pd-2" key={pageTag.id}>
-                {pageTag.name}
+                {pageTag.name} /
               </div>
             )})}
           </div>
           <div className="d-flex justify-content-center" >
-            <div className="p-2" onClick={() => this.discussClicked()}>Discuss</div>
-            <div className="p-2">Rate</div>
-            <div className="p-2">Edit</div>
+            <div className="p-2" onClick={() => this.discussClicked()}>
+              <button type="button" className="btn btn-link">
+                Discuss
+              </button>
+            </div>
+            <div className="p-2">
+              <button type="button" className="btn btn-link">
+                Rate
+              </button>
+            </div>
+            <div className="p-2">
+              <button type="button" className="btn btn-link">
+                Edit
+              </button>
+            </div>
           </div>
           <div className="Discussion col-md-8 mx-auto" id="discussion">
             <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
@@ -132,7 +225,7 @@ class Page extends Component {
               of this page is licensed under <br />
               <a href="http://creativecommons.org/licenses/by-sa/3.0/"
               target="_blank" rel="noopener noreferrer">
-                Creative Commons Attribution-ShareAlike 3.0 License
+                Creative Commons Attribution-ShareAlike 3.0 License.
               </a>
             </div>
           </div>
