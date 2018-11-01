c5c54f2f90daa850ad6295df8c79cfa88216275a
Aidan Low <aidan.low98@gmail.com>
2018-10-31 22:40:08 -0400

    convert to hashrouter to support gh-pages routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Route path="/" component={App} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
